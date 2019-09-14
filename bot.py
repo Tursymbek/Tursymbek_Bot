f795d29be8618f4f607794acd85328ec362eeed4
Shynggys <tursymbeksn@gmail.com>
2019-09-14 12:27:32 +0600

    Add new function /cat

--- bot.py
+++ bot.py
@@ -1,8 +1,13 @@
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from datetime import datetime
+from glob import glob
 import logging
-import settings
+from random import choice
+
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import ephem
-from datetime import datetime 
+from functools import wraps
+
+import settings
 
 logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
                     level = logging.INFO,
@@ -13,21 +18,26 @@ def greet_user(update, context):
     print (text)
     update.message.reply_text(text)
 
-def talk_to_me(update, context):
-    user_text = "Hello {}! You wrote: {}. \nYou can find these planets: \n Mercury, \n Venus, \n Mars, \n Jupiter, \n Saturn, \n Uranus, \n Neptune, \n Pluto, \n Sun, \n Moon.".format(update.message.chat.first_name, update.message.text)
-    logging.info("User: %s, Chat id: %s, Message: %s", update.message.chat.username, 
-                update.message.chat.id, update.message.text)
-    update.message.reply_text(user_text)
-
 def planets(update, context):      
-    user_planet = update.message.text
+    user_planet = update.message.text.split()
    
     u = getattr(ephem, user_planet[1])
     tm = datetime.now()
     star_in = u(tm)
     result = ephem.constellation(star_in)
-    rpl = str(result)
-    update.message.reply_text(" Your planet in {}".format(rpl))
+    update.message.reply_text("Your planet in {}.".format(result))
+
+def send_cat_picture(update, context):
+    cat_list = glob('image/cat*.jp*g')
+    cat_pic = choice(cat_list)
+    update.message.reply_photo(photo=open(cat_pic, 'rb'))
+    #bot.send_photo(chat_id=update.message.chat.id, photo = open(cat_pic, 'rb'))
+
+def talk_to_me(update, context):
+    user_text = "Hello {}! You wrote: {}. \nYou can find these planets: \n Mercury, \n Venus, \n Mars, \n Jupiter, \n Saturn, \n Uranus, \n Neptune, \n Pluto, \n Sun, \n Moon.".format(update.message.chat.first_name, update.message.text)
+    logging.info("User: %s, Chat id: %s, Message: %s", update.message.chat.username, 
+                update.message.chat.id, update.message.text)
+    update.message.reply_text(user_text)
 
 def main():
 
@@ -36,10 +46,13 @@ def main():
     logging.info('Bot is started')
 
     dp = updater.dispatcher
-    dp.add_handler(CommandHandler('start', greet_user))
-    dp.add_handler(MessageHandler(Filters.text, talk_to_me))
-    dp.add_handler(CommandHandler('planet', planets))
 
+    dp.add_handler(CommandHandler('start', greet_user))
+    dp.add_handler(CommandHandler('planet', planets))
+    dp.add_handler(CommandHandler('cat', send_cat_picture))
+
+    dp.add_handler(MessageHandler(Filters.text, talk_to_me))
+  
     updater.start_polling()
     updater.idle()
 
